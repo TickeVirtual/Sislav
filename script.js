ffd2c670e29cef1330b2cf214f4cb6ed3610f51d
SistemApp <147223054+TickeVirtual@users.noreply.github.com>
2025-06-21 16:58:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@
         sendMessageButton.disabled = true; // Deshabilitar botón mientras se envía
 
         const url = "https://mensajero-evolution-api.ykf6ye.easypanel.host/message/sendMedia/botinstancia"; // Cambia NOMBRE_INSTANCIA
-        const apikey = "63F783149485-491F-A852-0EAE47DE2ssss"; // Coloca aquí tu API key
+        const apikey = "9E3F31BC6102-4E56-BA66-2B3F3637A9D7"; // Coloca aquí tu API key
         const numeroTelefono = `+51${telefono}`; // Coloca el número de teléfono del destinatario
         const longURL = window.location.href; // Obtiene la URL actual
 
